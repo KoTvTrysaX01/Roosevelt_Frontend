083cf3c6a74f1a5ef506ff77b0de76766366ea18
Elshin Vadim <elshinvadim@gmail.com>
2026-05-02 23:13:32 +0200

    Router & Updated Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { MyRoutes } from './pages/my-routes/my-routes';
+import { FavRoutes } from './pages/fav-routes/fav-routes';
+import { Settings } from './pages/settings/settings';
+import { Administrador } from './pages/administrador/administrador';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'map',
+    component: Map,
+  },
+  {
+    path: 'my_routes',
+    component: MyRoutes,
+  },
+  {
+    path: 'fav_routes',
+    component: FavRoutes,
+  },
+  {
+    path: 'settings',
+    component: Settings,
+  },
+  {
+    path: 'admin',
+    component: Administrador,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,15 @@ import { Header } from "./components/header/header";
 import { Navbar } from "./components/navbar/navbar";
 import { Footer } from "./components/footer/footer";
 import { Home } from "./pages/home/home";
+import { FavRoutes } from "./pages/fav-routes/fav-routes";
+import { MyRoutes } from "./pages/my-routes/my-routes";
+import { Map } from "./pages/map/map";
+import { Settings } from "./pages/settings/settings";
+import { Administrador } from "./pages/administrador/administrador";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Navbar, Footer, Home],
+  imports: [RouterOutlet, Header, Navbar, Footer, Home, FavRoutes, MyRoutes, Map, Settings, Administrador],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
