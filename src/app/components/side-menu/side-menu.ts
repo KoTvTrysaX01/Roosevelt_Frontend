40b651015265f62e84290cf2c0404d59cd74d66d
Elshin Vadim <elshinvadim@gmail.com>
2026-05-09 18:30:34 +0200

    Worked on side-menu & updated styles.scss

--- src/app/components/side-menu/side-menu.ts
+++ src/app/components/side-menu/side-menu.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './side-menu.html',
   styleUrl: './side-menu.scss',
 })
 export class SideMenu {
+  menuLabel: string | undefined;
+  visible: boolean = true;
 
 }
