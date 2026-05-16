c5be7ec6ffb7ac7b9a2b589c02d2eb37dcb7081f
Elshin Vadim <elshinvadim@gmail.com>
2026-05-17 01:50:09 +0200

    Extra protection in Admin page

--- src/app/pages/administrador/administrador.ts
+++ src/app/pages/administrador/administrador.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SideMenu } from "../../components/side-menu/side-menu";
-import { RouterOutlet } from "@angular/router";
+import { Router, RouterOutlet } from "@angular/router";
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-administrador',
@@ -8,6 +9,16 @@ import { RouterOutlet } from "@angular/router";
   templateUrl: './administrador.html',
   styleUrl: './administrador.scss',
 })
-export class Administrador {
+export class Administrador implements OnInit{
+
+  router = inject(Router)
+  authService = inject(AuthService)
+  ngOnInit(): void {
+    if(this.authService.currentUserSig()?.roles !== '[ROLE_ADMIN]'){
+      this.router.navigateByUrl('/')
+    }
+  }
+
+  
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // API_ROOSEVELT: 'http://54.196.40.74:8080/roosevelt/api'
-    API_ROOSEVELT: '/roosevelt'
+    API_ROOSEVELT: 'http://54.196.40.74:8080/roosevelt/api'
+    // API_ROOSEVELT: '/roosevelt'
 };
